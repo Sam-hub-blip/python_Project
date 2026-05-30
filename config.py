204be0da6a12618cb40a7b3955a4a0a7170ce02f
Tinwinde Samuel Zongo <tinwindesamuelzongo@gmail.com>
2026-05-30 20:05:57 +0000

    Setup application configuration and core execution loop

---
+++ config.py
@@ -0,0 +1,11 @@
+from typing import Tuple
+
+HOSPITAL_NAME = "GOSEN"
+SEVERE_CASES: Tuple[str, ...] = (
+    "heart attack",
+    "hemorrhage",
+    "stroke",
+    "major trauma",
+    "surgery",
+)
+MAX_QUEUE_SIZE = 3
